62b95131ab7945c50565b78ebacc469760a014eb
Haojian Jin <haojian.jin@gmail.com>
2013-02-20 11:55:12 -0800

    update

---
+++ src/lang_processor.java
@@ -0,0 +1,12 @@
+
+public class lang_processor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/processing/Extraction_bootstrapping.java
@@ -0,0 +1,13 @@
+package processing;
+
+public class Extraction_bootstrapping {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
