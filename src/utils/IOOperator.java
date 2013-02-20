1175e7009df1d63bb7d348ce64b96033b3af0d0d
Haojian Jin <haojian.jin@gmail.com>
2013-02-20 12:04:50 -0800

    update

--- src/utils/IOOperator.java
+++ src/utils/IOOperator.java
@@ -1,5 +1,37 @@
 package utils;
 
 public class IOOperator {
+	private static IOOperator singleton;
+	private FileWriter writer;
+	public String writeToFilePath;
 
+	public IOOperator(){
+		writeToFilePath = "";
+	}
+	
+	public static IOOperator getInstance() {
+		if (singleton == null)
+			singleton = new IOOperator();
+		return singleton;
+	}
+	
+	public void closeStream(){
+		try {
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void writeToFile(String filename, String content, boolean isappend){
+		try {
+			writer = new FileWriter(filename, isappend);
+			writer.write(content);
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
