392eb196e0a1ff58b58c6c8460ad67150a246e7a
Haojian Jin <haojian.jin@gmail.com>
2013-02-28 17:57:33 -0800

    update

--- src/processing/Extraction_bootstrapping.java
+++ src/processing/Extraction_bootstrapping.java
@@ -93,7 +93,7 @@ public class Extraction_bootstrapping {
 			lastIterationSize = extractionMap.size();
 			
 			TemplateInduction();
-			//AttributeInduction();
+			AttributeInduction();
 			ValueInduction();
 			UpdateToNewIteration();
 			i++;
