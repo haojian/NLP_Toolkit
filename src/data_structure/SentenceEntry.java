174c66a7177c401818597ccfe1e98f63f6b04557
Haojian Jin <haojian.jin@gmail.com>
2013-03-12 15:17:27 -0700

    update

--- src/data_structure/SentenceEntry.java
+++ src/data_structure/SentenceEntry.java
@@ -1,5 +1,16 @@
 package data_structure;
 
-public class SentenceEntry {
+import java.util.HashMap;
+import java.util.Map;
 
+public class SentenceEntry {
+	private String _senttxt = "";
+	public Map<Template, Extraction> Extractionmap;
+	
+	public SentenceEntry(String sent){
+		_senttxt = sent;
+		Extractionmap = new HashMap<Template, Extraction>();
+	}
+	
+	
 }
