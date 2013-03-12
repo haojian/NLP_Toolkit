44030a8da36e33d714d47f15c8f8b3eaa974b45c
Haojian Jin <haojian.jin@gmail.com>
2013-03-12 15:41:33 -0700

    update

--- src/data_structure/SentenceEntry.java
+++ src/data_structure/SentenceEntry.java
@@ -1,15 +1,26 @@
 package data_structure;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SentenceEntry {
 	private String _senttxt = "";
+	public ArrayList<Template> CandidateTemplates;
 	public Map<Template, Extraction> Extractionmap;
 	
 	public SentenceEntry(String sent){
-		_senttxt = sent;
+		set_senttxt(sent);
+		CandidateTemplates = new ArrayList<Template>();
 		Extractionmap = new HashMap<Template, Extraction>();
+	}
+
+	public String get_senttxt() {
+		return _senttxt;
+	}
+
+	public void set_senttxt(String _senttxt) {
+		this._senttxt = _senttxt;
 	}
 	
 	
